8052e199f96f333ddf5dfcff1ba147b7eec883a8
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-03-26 14:56:42 +0530

    feat: step 9

--- src/index.js
+++ src/index.js
@@ -1,30 +1,122 @@
-const parseQuery = require("./queryParser");
+const { parseQuery } = require("./queryParser");
 const readCSV = require("./csvReader");
 
+async function performInnerJoin(data, joinData, joinCondition, fields, table) {
+  return (data = data.flatMap((mainRow) => {
+    return joinData
+      .filter((joinRow) => {
+        const mainValue = mainRow[joinCondition.left.split(".")[1]];
+        const joinValue = joinRow[joinCondition.right.split(".")[1]];
+        return mainValue === joinValue;
+      })
+      .map((joinRow) => {
+        return fields.reduce((acc, field) => {
+          const [tableName, fieldName] = field.split(".");
+          acc[field] =
+            tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+          return acc;
+        }, {});
+      });
+  }));
+}
+async function performLeftJoin(data, joinData, joinCondition, fields, table) {
+  return (data = data.flatMap((mainRow) => {
+    const matchingJoinRows = joinData.filter((joinRow) => {
+      const mainValue = mainRow[joinCondition.left.split(".")[1]];
+      const joinValue = joinRow[joinCondition.right.split(".")[1]];
+      return mainValue === joinValue;
+    });
+
+    if (matchingJoinRows.length === 0) {
+      // If there are no matching rows in joinData, create a result with nulls for joinData fields
+      return {
+        ...mainRow,
+        ...fields.reduce((acc, field) => {
+          const [tableName, fieldName] = field.split(".");
+          acc[field] = tableName === table ? mainRow[fieldName] : null;
+          return acc;
+        }, {}),
+      };
+    }
+
+    return matchingJoinRows.map((joinRow) => {
+      return fields.reduce((acc, field) => {
+        const [tableName, fieldName] = field.split(".");
+        acc[field] =
+          tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+        return acc;
+      }, {});
+    });
+  }));
+}
+
+async function performRightJoin(data, joinData, joinCondition, fields, table) {
+  return (joinData = joinData.flatMap((joinRow) => {
+    const matchingDataRows = data.filter((mainRow) => {
+      const mainValue = mainRow[joinCondition.left.split(".")[1]];
+      const joinValue = joinRow[joinCondition.right.split(".")[1]];
+      return mainValue === joinValue;
+    });
+
+    if (matchingDataRows.length === 0) {
+      return {
+        ...joinRow,
+        ...fields.reduce((acc, field) => {
+          const [tableName, fieldName] = field.split(".");
+          acc[field] = tableName === table ? null : joinRow[fieldName];
+          return acc;
+        }, {}),
+      };
+    }
+
+    return matchingDataRows.map((mainRow) => {
+      return fields.reduce((acc, field) => {
+        const [tableName, fieldName] = field.split(".");
+        acc[field] =
+          tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+        return acc;
+      }, {});
+    });
+  }));
+}
+
 async function executeSELECTQuery(query) {
-  const { fields, table, whereClauses, joinTable, joinCondition } =
+  const { fields, table, whereClauses, joinType, joinTable, joinCondition } =
     parseQuery(query);
+
   let data = await readCSV(`${table}.csv`);
 
-  // Perform INNER JOIN if specified
   if (joinTable && joinCondition) {
     const joinData = await readCSV(`${joinTable}.csv`);
-    data = data.flatMap((mainRow) => {
-      return joinData
-        .filter((joinRow) => {
-          const mainValue = mainRow[joinCondition.left.split(".")[1]];
-          const joinValue = joinRow[joinCondition.right.split(".")[1]];
-          return mainValue === joinValue;
-        })
-        .map((joinRow) => {
-          return fields.reduce((acc, field) => {
-            const [tableName, fieldName] = field.split(".");
-            acc[field] =
-              tableName === table ? mainRow[fieldName] : joinRow[fieldName];
-            return acc;
-          }, {});
-        });
-    });
+    switch (joinType.toUpperCase()) {
+      case "INNER":
+        data = await performInnerJoin(
+          data,
+          joinData,
+          joinCondition,
+          fields,
+          table
+        );
+        break;
+      case "LEFT":
+        data = await performLeftJoin(
+          data,
+          joinData,
+          joinCondition,
+          fields,
+          table
+        );
+        break;
+      case "RIGHT":
+        data = await performRightJoin(
+          data,
+          joinData,
+          joinCondition,
+          fields,
+          table
+        );
+        break;
+    }
   }
 
   const filterData = () => {
@@ -34,6 +126,7 @@ async function executeSELECTQuery(query) {
         )
       : data;
   };
+
   const filteredData = filterData();
 
   try {
